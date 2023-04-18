cefc1e76130a1ae5c6cbe851b3bf205e794fce28
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-04-17 18:19:41 -0600

    added test

---
+++ src/components/vanilla/ARComponents.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect} from "react"
+import ARExperience from "./Experience"
+
+const ARComponents = () => {
+    const arExperience = new ARExperience()
+    useEffect(() => {
+        arExperience.initScene()
+        arExperience.setupARExperience()
+        arExperience.loadModel()
+    }, [])
+    return (
+        <div
+            className="container3D"
+            style={{
+                marginTop: "100px",
+                width: "100%",
+                height: "100vh"
+            }}
+        >
+vsdfgbdfhbdgfjnd
+        </div>
+    )
+}
+
+export default ARComponents;
